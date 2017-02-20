6e1af574bf7a89f022651890d6149fb381dac1b4
HutEight <sxh759@case.edu>
2017-02-20 08:50:05 -0500

    Bugs and old test files removed from gearbox

--- src/playfile_gearbox.cpp
+++ src/playfile_gearbox.cpp
@@ -17,6 +17,7 @@
 
 using namespace std;
 
+//Simple Version
 //bool Playfile_gearbox::change_gear (double& scale_, std::vector<double> &arrival_times_, int data_size_) {
 	//cout << "data_size_ is: "<< data_size_ <<"\n";
 //	for (int i=0 ; i< data_size_; i++){
@@ -27,21 +28,38 @@ using namespace std;
 //	return 1;
 //}
 
+//Advanecd Version
 bool Playfile_gearbox::change_gear (double& scale_, std::vector<double> &arrival_times_, int data_size_){
 	int line_0, line_n;
 	ROS_INFO("GEARBOX..");
 	cout << "There are "<< data_size_ << " rows inside this playfile, please specify from which line you wish to scale: \n";
 	cin >> line_0;
 	cout << "Please specify which line you wish to end scaling: \n";
+	//cin.clear();
 	cin >> line_n;
-	
-	for (int i =0; i < (line_0 -1); i++) {
-		new_arrival_times.push_back( arrival_times_[i] ); }
-	for (int i = (line_0-1); i <= (line_n-1); i++) {
+	cout << "line 0 is: " <<line_0 << "and line_n is: " << line_n << "\n"; //TODO delete
+	double base_t = arrival_times_[line_0 -1 ];
+
+	for (int i =0; i < (line_0); i++) {
+		//cout << "arrival_times_[i]" << i << ": " <<arrival_times_[i] <<"\n"; //debug use
+		//note that the fisrt row never changes to ensure enough time for gripper to get in position
+		new_arrival_times.push_back( arrival_times_[i] ); 
+
+		//cout << "new_arrival_times_[i]" << i << ": " << new_arrival_times[i] <<"\n";
+	}
+
+	for (int i = (line_0); i <= (line_n-1); i++) {
 		if (line_0 > 1){
-		new_arrival_times.push_back( (arrival_times_[i] - arrival_times_[line_0 -1 ])/scale_  + arrival_times_[line_0 -1 ]);}
-		if (line_0 =1 ){
-		new_arrival_times.push_back( (arrival_times_[i])/scale_  + arrival_times_[line_0 -1 ]);}}
+		//cout << "arrival_times_[i]" << i << ": " <<arrival_times_[i] <<"\n";
+		
+		new_arrival_times.push_back( (arrival_times_[i] - base_t)/scale_  + base_t);
+
+		//cout << "new_arrival_times_[i]" << i << ": " <<new_arrival_times[i] <<"\n";
+		}
+		
+		else if (line_0 =1 ){
+		new_arrival_times.push_back( (arrival_times_[i]-arrival_times_[0])/scale_  + arrival_times_[0]);}
+	}
 
 	for (int i = line_n; i < data_size_; i++) {
 		new_arrival_times.push_back( arrival_times_[i] - arrival_times_[line_n - 1] + new_arrival_times[line_n - 1]); }
